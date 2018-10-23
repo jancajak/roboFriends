090139023e5a5a183621af5e2005a0ddcd611610
Jakub Janca <janca.jake@gmail.com>
2018-10-23 15:57:11 +0200

    Updates

---
+++ precache-manifest.6405ebcbc729a456a3f32ae5f99149b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1a95c78c8b1e07287e07",
+    "url": "/roboFriends/static/js/runtime~main.1a95c78c.js"
+  },
+  {
+    "revision": "b67e88cf7b632b1102f2",
+    "url": "/roboFriends/static/js/main.b67e88cf.chunk.js"
+  },
+  {
+    "revision": "d5c97c648570eccf3dec",
+    "url": "/roboFriends/static/js/1.d5c97c64.chunk.js"
+  },
+  {
+    "revision": "b67e88cf7b632b1102f2",
+    "url": "/roboFriends/static/css/main.d23704f0.chunk.css"
+  },
+  {
+    "revision": "d5c97c648570eccf3dec",
+    "url": "/roboFriends/static/css/1.75588dcc.chunk.css"
+  },
+  {
+    "revision": "49554674ea0b5f6b01600719cb04265f",
+    "url": "/roboFriends/index.html"
+  }
+];

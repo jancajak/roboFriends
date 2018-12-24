c8b317786a655ef3e599df4695c8a421ba911ddc
C:\Program Files\Git <lhotecky@reinto.cz>
2018-12-24 19:54:48 +0100

    Updates

---
+++ precache-manifest.2007e31e619668bac51d754ac5012ace.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1a95c78c8b1e07287e07",
+    "url": "/roboFriends/static/js/runtime~main.1a95c78c.js"
+  },
+  {
+    "revision": "09db9cbd25fab4b6e7b7",
+    "url": "/roboFriends/static/js/main.09db9cbd.chunk.js"
+  },
+  {
+    "revision": "df8820d94e7c2dc56d51",
+    "url": "/roboFriends/static/js/1.df8820d9.chunk.js"
+  },
+  {
+    "revision": "09db9cbd25fab4b6e7b7",
+    "url": "/roboFriends/static/css/main.ea36cb8d.chunk.css"
+  },
+  {
+    "revision": "df8820d94e7c2dc56d51",
+    "url": "/roboFriends/static/css/1.f813fd6d.chunk.css"
+  },
+  {
+    "revision": "b812521aa5ec3c89f5f926d5913f7e8d",
+    "url": "/roboFriends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6387a34012275a4bdc0faab3e502e974.js"
+  "/roboFriends/precache-manifest.2007e31e619668bac51d754ac5012ace.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/roboFriends/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
